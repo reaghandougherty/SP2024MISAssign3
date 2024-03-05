62cefac9c9da16aa7627d659c474870c2bc38a44
Reaghan Dougherty <rwdougherty@crimson.ua.edu>
2024-03-05 14:55:48 -0600

    movie cols added

---
+++ TheMovieDB/Migrations/20240305205331_moviecols.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheMovieDB.Migrations
+{
+    public partial class moviecols : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Genre",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Poster",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Year",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Genre",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "Poster",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "Year",
+                table: "Movie");
+        }
+    }
+}
--- TheMovieDB/Models/Movie.cs
+++ TheMovieDB/Models/Movie.cs
@@ -5,6 +5,9 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string IMDB { get; set; }
+        public string Genre { get; set; }
+        public string Year { get; set; }
+        public string Poster { get; set; }
         public string Description { get; set; }
         public string Director { get; set; }
 
